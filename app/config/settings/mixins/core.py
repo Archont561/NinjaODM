cd66fff09366d57a8261d8089a8ec03d41f7842c
Eryk Srebrny <esrebrny@onet.eu>
2025-10-05 17:01:21 +0200

    Fix Django module paths - use app.config prefix consistently

--- app/config/settings/mixins/core.py
+++ app/config/settings/mixins/core.py
@@ -8,7 +8,7 @@ class CoreSettingsMixin(BaseSettings):
 
     ALLOWED_HOSTS: List[str] = Field(default=["localhost", "127.0.0.1"])
 
-    ROOT_URLCONF: str = "config.urls"
-    WSGI_APPLICATION: str = "config.wsgi.application"
-    ASGI_APPLICATION: str = "config.asgi.application"
+    ROOT_URLCONF: str = "app.config.urls"
+    WSGI_APPLICATION: str = "app.config.wsgi.application"
+    ASGI_APPLICATION: str = "app.config.asgi.application"
     DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
