95d16165f7b44fadfc3917f86fae144862625267
Eryk Srebrny <esrebrny@onet.eu>
2025-10-05 21:15:46 +0200

    feat(settings): add custom validator for ALLOWED_HOSTS to get them from comma separated environment variable

--- app/config/settings/mixins/core.py
+++ app/config/settings/mixins/core.py
@@ -1,7 +1,6 @@
 from typing import List
 
-from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic import Field, field_validator
 
 from .base import BaseSettingsMixin
 
@@ -9,6 +8,19 @@ from .base import BaseSettingsMixin
 class CoreSettingsMixin(BaseSettingsMixin):
 
     ALLOWED_HOSTS: List[str] = Field(default=["localhost", "127.0.0.1"])
+
+    @field_validator("ALLOWED_HOSTS", mode="before")
+    @classmethod
+    def split_csv_to_list(cls, v):
+        if isinstance(v, str):
+            return [item.strip() for item in v.split(",")]
+
+        if isinstance(v, list):
+            return v
+
+        raise ValueError(
+            f"ALLOWED_HOSTS must be comma separated string or list or strings, not {v}!"
+        )
 
     ROOT_URLCONF: str = "app.config.urls"
     WSGI_APPLICATION: str = "app.config.wsgi.application"
