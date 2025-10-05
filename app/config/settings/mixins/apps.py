b36f9855fac28f9d1228a0ba8a80796e49ff692f
Eryk Srebrny <esrebrny@onet.eu>
2025-10-05 12:54:11 +0200

    fix(app): change name of core app to "app.core" to prevent type checking errors

--- app/config/settings/mixins/apps.py
+++ app/config/settings/mixins/apps.py
@@ -9,25 +9,6 @@ class AppsSettingsMixin(BaseSettings):
     @computed_field
     @property
     def INSTALLED_APPS(self) -> List[str]:
-        return [
-            # Django core
-            'django.contrib.admin',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.messages',
-            'django.contrib.staticfiles',
-            'django.contrib.gis',
-        ] + [
-            # Third party
-            'corsheaders',
-            'ninja_extra',
-            'ninja_jwt',
-            'ninja_jwt.token_blacklist',
-        ] + [
-            # Local apps
-            'core',
-        ]
         return (
             [
                 # Django core
@@ -48,5 +29,6 @@ class AppsSettingsMixin(BaseSettings):
             ]
             + [
                 # Local apps
+                "app.core",
             ]
         )
