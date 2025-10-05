bf9e6cec1649b8ad7c44575888d78cf4c1b23c51
Eryk Srebrny <esrebrny@onet.eu>
2025-10-05 05:44:05 +0200

    test: create basic blueprint for tests (fixtures/factories, unit, integration, e2e) and test app settings

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from ninja_extra.testing import TestClient
+from app.config.settings.main import PydanticDjangoSettings
+
+from app.api import api
+
+
+@pytest.fixture
+def api_client(scope='session'):
+    """Create a test client for the API"""
+    api.auto_discover_controllers()
+    return TestClient(api)
+
+@pytest.fixture(scope="session")
+def test_settings():
+    return PydanticDjangoSettings(ENVIRONMENT="test") # noqa
