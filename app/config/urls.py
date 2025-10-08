07a8c6dcd2e93ac12d4beb1112049aeb5d8a3577
Eryk Srebrny <esrebrny@onet.eu>
2025-10-08 15:24:09 +0200

    feat(settings): serve static files if DEBUG is true

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,9 +1,17 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 from app.api import api
 
+from .settings.main import get_settings
+
+settings = get_settings()
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
