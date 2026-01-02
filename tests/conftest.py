5d283f8389121494148bb831b94afd28091d3cc5
Eryk Srebrny <esrebrny@onet.eu>
2026-01-02 22:22:02 +0100

    test(odm): add fixture and factory for Image model

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,18 @@ import pytest
 from ninja_extra.testing import TestClient
 from pytest_factoryboy import register
 
+from ninja_jwt.tokens import AccessToken
+from pathlib import Path
+
 from app.api.api import create_api
 from app.config.settings.main import get_settings
-from ninja_jwt.tokens import AccessToken
 
-from tests.factories import WorkspaceFactory
+
+from tests.factories import WorkspaceFactory, ImageFactory
 
 
 register(WorkspaceFactory)
+register(ImageFactory)
 
 
 @pytest.fixture(scope="session")
@@ -31,4 +35,13 @@ def valid_token():
     token["exp"] = 9999999999
     token["iat"] = 1600000000
     return token
-    
+
+
+@pytest.fixture
+def temp_media(tmp_path, settings):
+    """
+    Override MEDIA_ROOT for tests to a temporary folder.
+    Automatically cleaned up after the test.
+    """
+    settings.MEDIA_ROOT = tmp_path
+    yield tmp_path
