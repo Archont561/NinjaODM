73996a9a34c467d77ac8ca53959cdb87cb0e65f2
Eryk Srebrny <esrebrny@onet.eu>
2026-01-06 04:13:37 +0100

    fix(auth): add missing import for TokenError

--- app/api/auth/user.py
+++ app/api/auth/user.py
@@ -1,5 +1,6 @@
 from ninja_jwt.authentication import JWTAuth
 from ninja_jwt.tokens import AccessToken
+from ninja_jwt.exceptions import TokenError
 from django.http import HttpRequest
 
 from app.api.constants.user import ServiceUser
