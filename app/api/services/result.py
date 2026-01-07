329b8e47ddf5f0c4d665f696da1452d73898ec8f
Eryk Srebrny <esrebrny@onet.eu>
2026-01-07 22:36:31 +0100

    feat(sse): implement SSE for ODMTaskResult model

--- app/api/services/result.py
+++ app/api/services/result.py
@@ -1,5 +1,16 @@
+from pathlib import Path
 from ninja_extra import ModelService
+
+from app.api.sse import emit_event
 
 
 class ResultModelService(ModelService):
-    ...
+    def delete(self, instance, **kwargs):
+        payload = {"uuid": str(instance.uuid), "result_type": instance.odm_result_type.label}
+        instance.delete()
+        file_path = Path(instance.file.path)
+        if file_path.exists():
+            file_path.unlink()
+
+        emit_event(instance.workspace.user_id, "task-result:deleted", payload)
+
