11c744e8cff325c54f88cc3f055155c7240c1dfe
Eryk Srebrny <esrebrny@onet.eu>
2026-01-07 20:06:22 +0100

    feat(sse): implement SSE for Workspace model

--- app/api/services/workspace.py
+++ app/api/services/workspace.py
@@ -9,12 +9,24 @@ from app.api.sse import emit_event
 class WorkspaceModelService(ModelService):
     def create(self, schema, **kwargs):
         instance = super().create(schema, **kwargs)
-        emit_event(
-            instance.user_id,
-            "workspace:created",
-            {"uuid": str(instance.uuid), "name": instance.name},
-        )
+        emit_event(instance.user_id, "workspace:created", {
+            "uuid": str(instance.uuid), 
+            "name": instance.name
+        })
         return instance
+
+    def update(self, instance, schema, **kwargs):
+        update_instance = super().update(instance, schema, **kwargs)
+        emit_event(update_instance.user_id, "workspace:updated", {
+            "uuid": str(instance.uuid), 
+            "name": instance.name
+        })
+        return update_instance
+
+    def delete(self, instance):
+        payload = {"uuid": str(instance.uuid), "name": instance.name}
+        instance.delete()
+        emit_event(instance.user_id, "workspace:deleted", payload)
 
     def save_images(self, instance, image_files: List[UploadedFile]):
         images = []
@@ -22,4 +34,9 @@ class WorkspaceModelService(ModelService):
             image = Image.objects.create(workspace=instance, image_file=image_file)
             image.make_thumbnail()
             images.append(image)
+        
+        emit_event(instance.user_id, "workspace:images-uploaded", {
+            "uuid": str(instance.uuid),
+            "uploaded": len(images),
+        })
         return images
