da5c329eb06fbfec7e01e4a18fab9af684641193
Eryk Srebrny <esrebrny@onet.eu>
2026-01-07 22:22:43 +0100

    feat(sse): implement SSE for GroundControlPoint  model

--- app/api/services/gcp.py
+++ app/api/services/gcp.py
@@ -2,19 +2,27 @@ import json
 from ninja_extra import ModelService
 from django.contrib.gis.geos import Point as GEOSPoint
 
+from app.api.sse import emit_event
+
 
 class GCPModelService(ModelService):
 
     def create(self, schema, **kwargs):
         image = kwargs.get("image")
         data = schema.model_dump()
-        return self.model.objects.create(
+        instance = self.model.objects.create(
             image=image,
             point=GEOSPoint(*data["gcp_point"], srid=4326),
             imgx=data["image_point"][0],
             imgy=data["image_point"][1],
             label=data["label"],
         )
+        
+        emit_event(instance.image.workspace.user_id, "gcp:created", {
+            "uuid": str(instance.uuid), 
+            "label": instance.label
+        })
+        return instance
 
     def update(self, instance, schema, **kwargs):
         data = schema.model_dump(exclude_unset=True)
@@ -25,7 +33,16 @@ class GCPModelService(ModelService):
         if "label" in data:
             instance.label = data["label"]
         instance.save()
+        emit_event(instance.image.workspace.user_id, "gcp:updated", {
+            "uuid": str(instance.uuid), 
+            "label": instance.label
+        })
         return instance
+
+    def delete(self, instance):
+        payload = {"uuid": str(instance.uuid), "label": instance.label}
+        instance.delete()
+        emit_event(instance.image.workspace.user_id, "gcp:deleted", payload)
 
     def queryset_to_geojson(self, queryset):
         qs = queryset.select_related('image').values(
