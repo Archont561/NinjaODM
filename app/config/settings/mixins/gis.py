31a38057611d0dee1001937e25c484228ac90884
Eryk Srebrny <esrebrny@onet.eu>
2026-01-07 10:08:48 +0100

    fix(settings): remove unecessary extension for SPATIALITE_LIBRARY_PATH when on linux

--- app/config/settings/mixins/gis.py
+++ app/config/settings/mixins/gis.py
@@ -5,20 +5,21 @@ from .base import BaseSettingsMixin
 
 
 class GISSettingsMixin(BaseSettingsMixin):
-
     @computed_field
     @property
     def PIXI_ENV_PATH(self) -> Path:
         return self.PROJECT_DIR / ".pixi" / "envs" / self.ENVIRONMENT
 
     @staticmethod
-    def _get_library_path(env_path: Path, lib_name: str) -> Path:
+    def _get_library_path(env_path: Path, lib_name: str, is_spatialite: bool = False) -> Path:
         """Return platform-specific library path."""
         if sys.platform == "win32":
             return env_path / "Library" / "bin" / f"{lib_name}.dll"
         elif sys.platform == "darwin":  # macOS
             return env_path / "lib" / f"lib{lib_name}.dylib"
-        else:  # Linux
+        elif is_spatialite:
+            return env_path / "lib" / lib_name
+        else:
             return env_path / "lib" / f"lib{lib_name}.so"
 
     @computed_field
@@ -39,4 +40,4 @@ class GISSettingsMixin(BaseSettingsMixin):
     @computed_field
     @property
     def SPATIALITE_LIBRARY_PATH(self) -> str:
-        return str(self._get_library_path(self.PIXI_ENV_PATH, "mod_spatialite"))
+        return str(self._get_library_path(self.PIXI_ENV_PATH, "mod_spatialite", is_spatialite=True))
