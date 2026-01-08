1707ed5d064645795663799f06f5b2a92dae490e
Eryk Srebrny <esrebrny@onet.eu>
2026-01-08 23:42:57 +0100

    feat(settings): add NINJAODM_BASE_URL and NODEODM_URL settings to ODMSettingsMixin

--- app/config/settings/mixins/odm.py
+++ app/config/settings/mixins/odm.py
@@ -14,6 +14,9 @@ class ODMSettingsMixin(BaseSettingsMixin):
     RESULTS_DIR_NAME: str = Field(default="results")
     GROUND_CONTROL_POINTS_FILE_NAME: str = Field(default="gcp_list.txt")
 
+    NINJAODM_BASE_URL: str = Field(...)
+    NODEODM_URL: str = Field(...)
+
     WORKSPACE_ALLOWED_FILE_MIME_TYPES: List[FILE_MIME_TYPE] = Field(
         default=[
             "image/jpeg",
