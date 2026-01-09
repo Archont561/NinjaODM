ac8b61243e91d3c9f77cbf3d27d6e84827049cf5
Eryk Srebrny <esrebrny@onet.eu>
2026-01-09 18:51:10 +0100

    feat(constants): move mappings for ODMTaskResultType and ODMProcessingStage to separate constants

--- app/api/constants/odm.py
+++ app/api/constants/odm.py
@@ -56,6 +56,62 @@ class ODMTaskStatus(ChoicesMixin, StrEnum):
         return self in self.terminal_states()
 
 
+ODM_TASK_RESULT_RELATIVE_PATHS_MAPPING: Dict[str, Path] = {
+        "POINT_CLOUD_PLY": Path("odm_georeferencing") / "odm_georeferenced_model.ply",
+        "POINT_CLOUD_LAZ": Path("odm_georeferencing") / "odm_georeferenced_model.laz",
+        "POINT_CLOUD_CSV": Path("odm_georeferencing") / "odm_georeferenced_model.csv",
+        "TEXTURED_MODEL": Path("odm_texturing") / "odm_textured_model.obj",
+        "TEXTURED_MODEL_GEO": Path("odm_texturing") / "odm_textured_model_geo.obj",
+        "ORTHOPHOTO_GEOTIFF": Path("odm_orthophoto") / "odm_orthophoto.tif",
+        "ORTHOPHOTO_WEBP": Path("odm_orthophoto") / "odm_orthophoto.webp",
+        "DSM": Path("odm_dem") / "dsm.tif",
+        "DTM": Path("odm_dem") / "dtm.tif",
+        "REPORT": Path("odm_georeferencing") / "odm_georeferencing_log.txt",
+    }
+
+
+@unique
+class ODMTaskResultType(ChoicesMixin, StrEnum):
+    POINT_CLOUD_PLY = auto()
+    POINT_CLOUD_LAZ = auto()
+    POINT_CLOUD_CSV = auto()
+    TEXTURED_MODEL = auto()
+    TEXTURED_MODEL_GEO = auto()
+    ORTHOPHOTO_GEOTIFF = auto()
+    ORTHOPHOTO_WEBP = auto()
+    DSM = auto()
+    DTM = auto()
+    REPORT = auto()
+
+    @property
+    def relative_path(self) -> Path:
+        try:
+            return ODM_TASK_RESULT_RELATIVE_PATHS_MAPPING[self.name]
+        except KeyError as exc:
+            raise NotImplementedError(
+                f"No relative path defined for {self}"
+            ) from exc
+
+
+ODM_PROCESSING_STAGE_RESULTS_MAPPING: Dict[str, List[ODMTaskResultType]] = {
+        "MVS_TEXTURING": [ODMTaskResultType.TEXTURED_MODEL],
+        "ODM_GEOREFERENCING": [
+            ODMTaskResultType.TEXTURED_MODEL_GEO,
+            ODMTaskResultType.POINT_CLOUD_PLY,
+            ODMTaskResultType.POINT_CLOUD_LAZ,
+            ODMTaskResultType.POINT_CLOUD_CSV,
+        ],
+        "ODM_DEM": [
+            ODMTaskResultType.DSM,
+            ODMTaskResultType.DTM,
+        ],
+        "ODM_ORTHOPHOTO": [
+            ODMTaskResultType.ORTHOPHOTO_GEOTIFF,
+            ODMTaskResultType.ORTHOPHOTO_WEBP,
+        ],
+        "ODM_REPORT": [ODMTaskResultType.REPORT],
+    }
+
 class ODMProcessingStage(ChoicesMixin, StrEnum):
     DATASET = auto()
     SPLIT = auto()
@@ -79,64 +135,9 @@ class ODMProcessingStage(ChoicesMixin, StrEnum):
         except IndexError:
             return None
 
-    _STAGE_RESULTS: Dict[str, List[ODMTaskResultType]] = {
-        "MVS_TEXTURING": [ODMTaskResultType.TEXTURED_MODEL],
-        "ODM_GEOREFERENCING": [
-            ODMTaskResultType.TEXTURED_MODEL_GEO,
-            ODMTaskResultType.POINT_CLOUD_PLY,
-            ODMTaskResultType.POINT_CLOUD_LAZ,
-            ODMTaskResultType.POINT_CLOUD_CSV,
-        ],
-        "ODM_DEM": [
-            ODMTaskResultType.DSM,
-            ODMTaskResultType.DTM,
-        ],
-        "ODM_ORTHOPHOTO": [
-            ODMTaskResultType.ORTHOPHOTO_GEOTIFF,
-            ODMTaskResultType.ORTHOPHOTO_WEBP,
-        ],
-        "ODM_REPORT": [ODMTaskResultType.REPORT],
-    }
-
     @property
     def stage_results(self) -> List[ODMTaskResultType]:
         try:
-            return self._STAGE_RESULTS[self.name]
+            return ODM_PROCESSING_STAGE_RESULTS_MAPPING[self.name]
         except KeyError:
             return []
-
-
-@unique
-class ODMTaskResultType(ChoicesMixin, StrEnum):
-    POINT_CLOUD_PLY = auto()
-    POINT_CLOUD_LAZ = auto()
-    POINT_CLOUD_CSV = auto()
-    TEXTURED_MODEL = auto()
-    TEXTURED_MODEL_GEO = auto()
-    ORTHOPHOTO_GEOTIFF = auto()
-    ORTHOPHOTO_WEBP = auto()
-    DSM = auto()
-    DTM = auto()
-    REPORT = auto()
-
-    _RELATIVE_PATHS: Dict[str, Path] = {
-        "POINT_CLOUD_PLY": Path("odm_georeferencing") / "odm_georeferenced_model.ply",
-        "POINT_CLOUD_LAZ": Path("odm_georeferencing") / "odm_georeferenced_model.laz",
-        "POINT_CLOUD_CSV": Path("odm_georeferencing") / "odm_georeferenced_model.csv",
-        "TEXTURED_MODEL": Path("odm_texturing") / "odm_textured_model.obj",
-        "TEXTURED_MODEL_GEO": Path("odm_texturing") / "odm_textured_model_geo.obj",
-        "ORTHOPHOTO_GEOTIFF": Path("odm_orthophoto") / "odm_orthophoto.tif",
-        "ORTHOPHOTO_WEBP": Path("odm_orthophoto") / "odm_orthophoto.webp",
-        "DSM": Path("odm_dem") / "dsm.tif",
-        "DTM": Path("odm_dem") / "dtm.tif",
-        "REPORT": Path("odm_georeferencing") / "odm_georeferencing_log.txt",
-    }
-
-    @property
-    def relative_path(self) -> Path:
-        try:
-            return self._RELATIVE_PATHS[self.name]
-        except KeyError as exc:
-            raise NotImplementedError(
-                f"No relative path defined for {self}"
-            ) from exc
