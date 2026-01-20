0ebf937f2e716435d2242c612df544dad5e94dd0
Eryk Srebrny <esrebrny@onet.eu>
2026-01-21 00:24:53 +0100

    feat(api): implement filtering by workspace_uuid in results api

--- app/api/schemas/result.py
+++ app/api/schemas/result.py
@@ -21,22 +21,4 @@ class ResultFilterSchema(FilterSchema):
     result_type: Optional[ODMTaskResultType] = Field(None, q="result_type")
     created_after: Optional[datetime] = Field(None, q="created_at__gte")
     created_before: Optional[datetime] = Field(None, q="created_at__lte")
-
-    @field_validator("result_type", mode="before")
-    @classmethod
-    def parse_enum_from_string(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            normalized_name = v.strip().replace(" ", "_").upper()
-            if v.isdigit():
-                return int(v)
-
-            try:
-                return ODMTaskResultType[normalized_name].value
-            except KeyError:
-                pass
-
-            raise ValueError(
-                f"Invalid Result Type: '{v}'. Accepted values: {[e.label for e in ODMTaskResultType]}"
-            )
-
-        return v
+    workspace_uuid: Optional[UUID] = Field(None, q="workspace__uuid")
