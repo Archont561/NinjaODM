b817a1194d80a4f9a2711d99d907635e3d12a025
Eryk Srebrny <esrebrny@onet.eu>
2026-01-21 00:37:57 +0100

    feat(api): implement filtering by workspace_uuid in tasls api

--- app/api/schemas/task.py
+++ app/api/schemas/task.py
@@ -59,41 +59,4 @@ class TaskFilterSchema(FilterSchema):
     step: Optional[ODMProcessingStage] = Field(None, q="step")
     created_after: Optional[datetime] = Field(None, q="created_at__gte")
     created_before: Optional[datetime] = Field(None, q="created_at__lte")
-
-    @field_validator("status", mode="before")
-    @classmethod
-    def parse_status_enum_from_string(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            normalized_name = v.strip().replace(" ", "_").upper()
-            if v.isdigit():
-                return int(v)
-
-            try:
-                return ODMTaskStatus[normalized_name].value
-            except KeyError:
-                pass
-
-            raise ValueError(
-                f"Invalid Task Status Type: '{v}'. Accepted values: {[e.label for e in ODMTaskStatus]}"
-            )
-
-        return v
-
-    @field_validator("step", mode="before")
-    @classmethod
-    def parse_step_enum_from_string(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            normalized_name = v.strip().replace(" ", "_").upper()
-            if v.isdigit():
-                return int(v)
-
-            try:
-                return ODMProcessingStage[normalized_name].value
-            except KeyError:
-                pass
-
-            raise ValueError(
-                f"Invalid Task Status Type: '{v}'. Accepted values: {[e.label for e in ODMProcessingStage]}"
-            )
-
-        return v
+    workspace_uuid: Optional[UUID] = Field(None, q="workspace__uuid")
